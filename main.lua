674b726b614f82f5ef5847f0e8aadbb468b76484
NotExplosive <wyatt@notexplosive.net>
2017-11-04 22:37:07 -0700

    Added rendering code; debug displays all players hands

--- main.lua
+++ main.lua
@@ -1,17 +1,33 @@
+love.math.setRandomSeed( os.time() )
+
 require('logic')
 require('game')
 require('test')
+require('render')
 
-g_dice = {}
+mainFont = love.graphics.newFont(12)
 
-for x=1, 5 do
-  --print("Player " .. x .. ": ")
-  for i=1,5 do
-    g_dice[#g_dice+1] = rollDi()
-    --print( g_dice[#g_dice] )
-  end
+currentGame = newGame()
+currentGame:setup(4,5)
+
+function love.update(dt)
+  -- body...
 end
 
-g_board = evaluateBoard(g_dice)
+function love.draw()
+  local y = 0
+  love.graphics.setColor(255,255,255)
+  love.graphics.setFont( mainFont )
 
---print(unpack(g_board))
+  for i = 1, #currentGame.players do
+    local name = currentGame.players[i].name
+    if i == currentGame.currentPlayerIndex then
+      name = name .. '*'
+    end
+    love.graphics.print(name,0,y)
+    y = y+love.graphics.getFont():getHeight()
+    drawHand(currentGame.players[i],0,y,32)
+    love.graphics.setColor(255,255,255)
+    y = y + 46
+  end
+end
---
+++ render.lua
@@ -0,0 +1,53 @@
+function drawDi(face,x,y,size)
+  love.graphics.setColor(200,200,200)
+  love.graphics.rectangle('fill', x, y, size, size)
+  love.graphics.setColor(0, 30, 0)
+
+  local circle_radius = 5/64 * size
+  if face % 2 == 1 then
+    love.graphics.circle('fill', x+size/2, y+size/2, circle_radius, 10)
+  end
+
+  -- unknown face
+  if face == 0 then
+    local font = love.graphics.newFont(size)
+    love.graphics.setFont( font )
+    local width = font:getWidth('?')
+    local height = font:getHeight('?')
+    love.graphics.print('?', x+size/2-width/2, y+size/2-height/2)
+  end
+
+  -- 3 5 2 4 6
+  if face > 1 then
+    love.graphics.circle('fill', x+size/4, y+size/4, circle_radius, 10)
+    love.graphics.circle('fill', x+size/4+size/2, y+size/4+size/2, circle_radius, 10)
+  end
+
+  -- 4 5 6
+  if face >= 4 then
+    love.graphics.circle('fill', x+size/2+size/4, y+size/4, circle_radius, 10)
+    love.graphics.circle('fill', x+size/4, y+size/2+size/4, circle_radius, 10)
+  end
+
+  -- 6
+  if face == 6 then
+    love.graphics.circle('fill', x+size/4, y+size/2, circle_radius, 10)
+    love.graphics.circle('fill', x+size-size/4, y+size/2, circle_radius, 10)
+  end
+
+  love.graphics.setColor(0,0,0,50)
+  love.graphics.circle('fill', x+size/2, y+size/2, size/2, 20)
+end
+
+function drawHand(player,x,y,scale)
+  if x == nil then
+    x = 0
+    y = 0
+  end
+  if scale == nil then
+    scale = 64
+  end
+  for i = 1, #player.hand do
+    drawDi(player.hand[i],x+(scale+4)*(i-1),y,scale)
+  end
+end
